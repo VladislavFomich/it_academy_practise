202467f268f46f45a30988ccecfd49245439bc45
Lonzelot <vladfomich123@gmail.com>
2020-11-13 18:32:47 +0300

    Create letter task

---
+++ LettersCount/LettersCount/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace LettersCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите слово:");
+            String word = Console.ReadLine();
+
+            int letter = 0;
+
+            for (int i = 0; i < word.Length; i++)
+            {
+
+                if (word[i] == 'a' | word[i] == 'а')
+                    letter++;
+            }
+            Console.WriteLine(letter);
+        }
+    }
+}
