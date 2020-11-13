bb68a01ba3ca35fb5ae0c21e50a34bdb732eef38
Lonzelot <vladfomich123@gmail.com>
2020-11-13 18:01:25 +0300

    Create clock task

---
+++ GoodDay/GoodDay/Greeting.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace GoodDay
+{
+  public class Greeting
+  {
+    public static void GreetingClass()
+    {
+            string nowTime = DateTime.Now.ToString("HH:mm");
+            Console.WriteLine(nowTime);
+
+            int nowHours = Convert.ToInt32(DateTime.Now.ToString("HH"));          
+
+            if(nowHours >= 9  &  nowHours <= 12)
+                Console.WriteLine("Good morning, guys");
+            
+            else if (nowHours >= 12 & nowHours <= 15)
+                Console.WriteLine("Good day, guys");
+
+
+            else if (nowHours >= 15 & nowHours <= 22)
+                    Console.WriteLine("Good evening, guys");
+
+           else
+                Console.WriteLine("Good night, guys");
+    }
+  }
+}
---
+++ GoodDay/GoodDay/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace GoodDay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Greeting.GreetingClass();
+        }
+    }   
+}
