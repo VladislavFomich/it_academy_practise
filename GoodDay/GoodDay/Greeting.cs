2f81630f5a6a36f0837a7a49ba35c9c3d9b59195
Lonzelot <vladfomich123@gmail.com>
2020-11-13 18:36:11 +0300

    fixed spaces

--- GoodDay/GoodDay/Greeting.cs
+++ GoodDay/GoodDay/Greeting.cs
@@ -9,23 +9,22 @@ namespace GoodDay
   {
     public static void GreetingClass()
     {
-            string nowTime = DateTime.Now.ToString("HH:mm");
-            Console.WriteLine(nowTime);
+          string nowTime = DateTime.Now.ToString("HH:mm");
+          Console.WriteLine(nowTime);
 
-            int nowHours = Convert.ToInt32(DateTime.Now.ToString("HH"));          
+          int nowHours = Convert.ToInt32(DateTime.Now.ToString("HH"));          
 
-            if(nowHours >= 9  &  nowHours <= 12)
-                Console.WriteLine("Good morning, guys");
+          if(nowHours >= 9  &  nowHours <= 12)
+              Console.WriteLine("Good morning, guys");
             
-            else if (nowHours >= 12 & nowHours <= 15)
-                Console.WriteLine("Good day, guys");
+          else if (nowHours >= 12 & nowHours <= 15)
+              Console.WriteLine("Good day, guys");
 
+          else if (nowHours >= 15 & nowHours <= 22)
+                  Console.WriteLine("Good evening, guys");
 
-            else if (nowHours >= 15 & nowHours <= 22)
-                    Console.WriteLine("Good evening, guys");
-
-           else
-                Console.WriteLine("Good night, guys");
+          else
+              Console.WriteLine("Good night, guys");
     }
   }
 }
