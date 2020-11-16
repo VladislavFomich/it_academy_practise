c7ded814d0d91912bbc24f419aade45db31fa106
Lonzelot <vladfomich123@gmail.com>
2020-11-16 20:31:23 +0300

    Created task1

---
+++ lecture_7/Lecture_7/Lecture_7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Lecture_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            task1();
+            task2();
+            task3();
+            task4();
+        }
+        static void task1()
+        {
+            object[] emptyArray = new object[] { };
+        }
+        static void task2()
+        {
+
+        }
+        static void task3()
+        {
+
+        }
+        static void task4()
+        {
+
+        }
+    }
+
+}
