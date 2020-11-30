7d7b768e948edbd01dd0dd2bd2081b638a0de2b3
Lonzelot <vladfomich123@gmail.com>
2020-11-30 19:59:37 +0300

    Created pratcise

---
+++ lecture_10/Motorcircle/Motorcircle/Motorcircle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Motorcircle
+{
+    internal class Motorcircle
+    {
+        public readonly int indeteficator;
+        public string model;
+        public string manufacturer;
+        public string yearRelease;
+        public int mileage;
+    }
+    class Engine : Motorcircle
+    {
+        public int volume;
+        public int power;
+    }
+}
---
+++ lecture_10/Motorcircle/Motorcircle/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Motorcircle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Motorcircle Minsk = new Motorcircle();
+            Engine MinskEngine = new Engine();
+            Minsk.model = Console.ReadLine();           
+            Minsk.manufacturer = Console.ReadLine();
+            Minsk.yearRelease = DateTime.Now.Year.ToString();
+            Minsk.mileage = Convert.ToInt32(Console.ReadLine());
+            MinskEngine.volume = Convert.ToInt32(Console.ReadLine());
+            MinskEngine.power = Convert.ToInt32(Console.ReadLine());
+
+            Motorcircle Honda = new Motorcircle();
+            Engine HondaEngine = new Engine();
+            Honda.model = Console.ReadLine();
+            Honda.manufacturer = Console.ReadLine();
+            Honda.yearRelease = DateTime.Now.Year.ToString();
+            Honda.mileage = Convert.ToInt32(Console.ReadLine());
+            HondaEngine.volume = Convert.ToInt32(Console.ReadLine());
+            HondaEngine.power = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Мотоцикл 1 - модель{Minsk.model}, проивз");
+            Console.WriteLine();
+        }
+    }
+}
