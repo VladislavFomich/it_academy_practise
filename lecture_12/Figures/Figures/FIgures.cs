2ab92557b8abb61d23323180c996e194867e5640
Lonzelot <vladfomich123@gmail.com>
2020-12-07 19:44:46 +0300

    practise

---
+++ lecture_12/Figures/Figures/FIgures.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Figures
+{
+    abstract class Figures
+    {
+       public int figuresSquare;
+       public int side;
+       public int radius;
+       public int height;
+       public const double pi = 3.14;
+    }
+       class Square : Figures
+    {
+         void SquareSquare()
+        {
+            int figuresSquare = side * side;
+        }
+    }
+        class Triangle : Figures
+    {
+        void TriangleSquare()
+        {
+            int figuresSquare = side * side;
+        }
+   }
+       class Circle : Figures
+    {
+        void CircleSquare()
+        {
+            double figuresSquare = pi * (radius * radius);
+        }
+    }
+}
