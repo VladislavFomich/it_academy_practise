af40d7f10f54733b0c98ecfe1772f886cb6d80be
Lonzelot <vladfomich123@gmail.com>
2020-12-14 19:40:26 +0300

    task2

---
+++ lecture_14/lecture14.Task2.Boxing/lecture14.Task2.Boxing/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+
+namespace lecture14.Task2.Boxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arrayList = new ArrayList();
+            
+            arrayList.Add(45);//int
+            arrayList.Add(true);//bool
+            arrayList.Add("Hello!");//string
+            arrayList.Add(23.45);//double
+
+        }
+    }
+}
