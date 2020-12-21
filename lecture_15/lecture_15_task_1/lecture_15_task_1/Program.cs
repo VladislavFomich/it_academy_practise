006658524b81cbf6abbc3c319799b6c3ea2944ac
Lonzelot <vladfomich123@gmail.com>
2020-12-21 19:17:15 +0300

    tadk1

---
+++ lecture_15/lecture_15_task_1/lecture_15_task_1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace lecture_15_task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UniqueItem uniqueItem = new UniqueItem();
+            uniqueItem.counter();
+            
+        }
+    }
+}
---
+++ lecture_15/lecture_15_task_1/lecture_15_task_1/UniqueItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lecture_15_task_1
+{
+    class UniqueItem
+    {
+      public static int id;
+
+        public void counter()
+        {
+           id = Convert.ToInt32(Console.ReadLine());
+           id++;
+            Console.WriteLine(id);
+        }
+    }
+}
