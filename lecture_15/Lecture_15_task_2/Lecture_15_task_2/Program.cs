12383ba4562ebcdd350157c54972ad786ff2c6cb
Lonzelot <vladfomich123@gmail.com>
2020-12-21 19:31:58 +0300

    Task_2

---
+++ lecture_15/Lecture_15_task_2/Lecture_15_task_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lecture_15_task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            str = str1;
+        }
+
+        public static class StringExtention
+        {
+            public static string strOutput(string str)
+            {
+                int str1;
+                for (int i = 0; i < 5; i++)
+                {
+                    str1[i] = str[i];
+                }
+
+                return str1;
+            }
+        }
+    }
+}
